ce4f7808664432acd31331b7e486566cef5f6fb1
daquiroz <daquiroz@uc.cl>
2017-06-08 11:27:40 -0400

    Deploy gh-pages from commit cd362e832fe14466ecc2690d79b785a754d2d19b

--- config/environment.js
+++ config/environment.js
@@ -186,7 +186,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     ENV.locationType = 'hash';
-    ENV.rootURL = '/';
+    ENV.rootURL = '/ddah-brasil';
   }
 
   return ENV;
